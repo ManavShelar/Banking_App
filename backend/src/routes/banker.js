0a5656a0c1a826c4fc3a4307bcfd9e0cefbadc3a
SumeetShelar <sumeetshelar2000@gmail.com>
2025-11-27 03:56:46 +0530

    solved render issue

--- backend/src/routes/banker.js
+++ backend/src/routes/banker.js
@@ -1,9 +1,6 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import {
-  getAllAccounts,
-  getUserTransactions,
-} from "../controllers/bankerController.js";
+import { getAllAccounts, getUserTransactions } from "../controllers/bankerController.js";
 
 const router = express.Router();
 router.use(authMiddleware);
